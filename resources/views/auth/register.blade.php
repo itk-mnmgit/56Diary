01f2c7aa910f06cfe9542250d755d373bdd07d03
Minamigaito Itsuki <itk.mnmgit@gmail.com>
2019-11-22 10:14:42 +0800

    registerに写真選択追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="picture" class="col-md-4 col-form-label text-md-right">Profile Picture</label>
+
+                            <div class="col-md-6">
+                                <input id="picture" type="file" class="form-control{{ $errors->has('picture') ? ' is-invalid' : '' }}" name="picture" required>
+
+                                @if ($errors->has('picture'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('picture') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
